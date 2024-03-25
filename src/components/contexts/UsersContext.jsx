8e053b30333e15d7c27a37d5e3647dc389e87b91
OvidijusSab <Ovidijuss1@gmail.com>
2024-03-25 11:16:09 +0200

    UsersContext added

---
+++ src/components/contexts/UsersContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useReducer, useState, useEffect } from "react";
+
+const UsersContext = createContext();
+
+export const UsersActionTypes = {
+  getAll: 'fetches all data on initial load'
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case UsersActionTypes.getAll:
+      return action.data
+    default:
+      console.error(`No such actions: ${action.type}`)
+      return state;
+  }
+}
+
+const UsersProvider = ({ children }) => {
+
+  const [users, setUsers] = useReducer(reducer, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/users`)
+      .then(res => res.json())
+      .then(data => setUsers({
+        type: UsersActionTypes.getAll,
+        data: data
+      }))
+  }, [])
+
+  return (
+    <UsersContext.Provider
+      value={{
+        users,
+        setUsers
+      }}
+    >
+      {children}
+    </UsersContext.Provider>
+  )
+
+}
+
+export { UsersProvider }
+export default UsersContext;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { CardsProvider } from './components/contexts/CardsContext';
+import { UsersProvider } from './components/contexts/UsersContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
+
     <BrowserRouter>
         <CardsProvider>
-            <App />
+            <UsersProvider>
+                <App />
+            </UsersProvider>
         </CardsProvider>
     </BrowserRouter>
 );
